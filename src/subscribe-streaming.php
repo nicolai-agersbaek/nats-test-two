49fde82985930ada8c0395618f57da747b420ca1
Nicolai Agersbæk <na@smartweb.dk>
2018-07-24 15:29:31 +0200

    Added example of subscribing on stream.

---
+++ src/subscribe-streaming.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types = 1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$options = new \NatsStreaming\ConnectionOptions();
+$options->setClientID('test');
+$options->setClusterID('test-cluster');
+
+$connection = new \NatsStreaming\Connection($options);
+
+$connection->connect();
+
+$subOptions = new \NatsStreaming\SubscriptionOptions();
+$subOptions->setStartAt(\NatsStreamingProtos\StartPosition::First());
+
+$subjects = 'some.subject';
+$callback = function ($message) {
+    \printf($message);
+};
+
+$sub = $connection->subscribe($subjects, $callback, $subOptions);
+
+$sub->wait(1);
+
+// not explicitly needed
+$sub->unsubscribe(); // or $sub->close();
+
+$connection->close();
